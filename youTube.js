630a0214fe9dc14c5f3ce2da17889385842915ce
Bakha022 <abduqahhorovbahodir536@gmail.com>
2023-05-19 14:40:13 +0500

    Youtube clonesi

---
+++ youTube.js
@@ -0,0 +1,112 @@
+let youtube = [
+    {
+        title:'Shaxnoz - Qaniydi',
+        video:'https://www.youtube.com/embed/LcaSMzYbZVU',
+        categories:'Music',
+    },
+    {
+        title:'Final Pursuit with Bugatti Veyron Super Sport - NFS MW',
+        video:'https://www.youtube.com/embed/IAT-lsuhuzo',
+        categories:'Game',
+    },
+    {    
+        title:'7 yilda 7ta Loyiha | Dasturchi Hikoyasi',
+        video:'https://youtube.com/embed/E4p0fiZXhl0',
+        categories:'IT',
+    },
+    {
+        title:'Ulugbek Rahmatullayev - Soginib',
+        video:'https://www.youtube.com/embed/-cBR33YjH4U',
+        categories:'Music',
+    },
+    {
+        title:'ASL WAYNE-GANIMAT',
+        video:'https://youtube.com/embed/dBRv7OxXuzA',
+        categories:'Music',
+    },
+    {
+        title:'Cristiano Ronaldo 50 Legendary Goals Impossible To Forget',
+        video:'https://youtube.com/embed/OUKGsb8CpF8',
+        categories:'Sport',
+    },
+    {
+        title:'Forza Horizon 5',
+        video:'https://youtube.com/embed/FYH9n37B7Yw',
+        categories:'Game',
+    },
+    {
+        title:'Counter-Strike: Global Offensive',
+        video:'https://youtube.com/embed/edYCtaNueQY',
+        categories:'Game',
+    },
+    {
+        title:'Ozbekistondagi talim illyuziyasi - Oliy talim | SUBYEKTIV',
+        video:'https://youtube.com/embed/eGp6VIVzyIk',
+        categories:'Ijtimoi',
+    },
+    {
+        title:'Git Tutorial for Beginners: Learn Git in 1 Hour',
+        video:'https://youtube.com/embed/8JJ101D3knE',
+        categories:'IT',
+    },
+    {
+        title:'MENTALITET — eng katta tosiq',
+        video:'https://www.youtube.com/embed/dEUfO1RKQyo',
+        categories:'Ijtimoi',
+    },
+    {
+        title:'Mittivine NmaGap | Harakatda barakat',
+        video:'https://youtube.com/embed/ex4ogBJpTwg',
+        categories:'Ijtimoi',
+    },
+    {
+        title:'Juventus - Real Madrid | 2017-18',
+        video:'https://www.youtube.com/embed/rWF9lJr9axo',
+        categories:'Sport',
+    },
+    {
+        title:'TOP 50 CS:GO Clips',
+        video:'https://youtube.com/embed/ltXs-ozqXY4',
+        categories:'Game',
+    },
+    {
+        title:'Ulugbek Rahmatullayev - Jonim',
+        video:'https://youtube.com/embed/EX5XXiivLTY',
+        categories:'Music',
+    },
+    {
+        title:'Java Full Course for free ☕',
+        video:'https://youtube.com/embed/xk4_1vDrzzo',
+        categories:'IT',
+    },
+    {
+        title:'Best Football Commercial 2022 Ft. Messi, Salah, Mbappe, Neymar, Ronaldinho, Pogba, Ronaldo...',
+        video:'https://youtube.com/embed/MRZFQshxxTo',
+        categories:'Sport',
+    },
+    {
+        title:'Shohruhxon - TikTok',
+        video:'https://youtube.com/embed/aA0ms5zwO-w',
+        categories:'Music',
+    },
+    {
+        title:'Ozbek futbol kelajagi bor Mirzahakim Toxtamirzayev | NmaGap',
+        video:'https://youtube.com/embed/DiZZKePTG2k',
+        categories:'Ijtimoi',
+    },
+    {
+        title:'JPRQ qanday ishlaydi?',
+        video:'https://www.youtube.com/embed/cXYiec-DLfo',
+        categories:'IT',
+    },
+    {
+        title:'EP!C Koenigsegg Jesko VS RAZOR + Final Pursuit - NFS MW',
+        video:'https://youtube.com/embed/c53CeF3AyWc',
+        categories:'Game',
+    },
+    {
+        title:'jprq ornatish | windows',
+        video:'https://youtube.com/embed/frgVQPi-GlY',
+        categories:'IT',
+    },
+]
